3258a7e972bad74057259e798285a2b3e30984ee
saranyaamurali <saranyaamurali05@gmail.com>
2026-06-03 23:31:53 +0530

    chore: support VITE_API_URL environment variable in api baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    "http://localhost:5000/api",
+    import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
 // Add interceptor to include token in headers
